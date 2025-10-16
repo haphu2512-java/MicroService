87ade16d47eea2f78eea12959a06164665aa6edb
haphu2512-java <haxuanphudeptrai123@gmail.com>
2025-10-16 15:40:10 +0700

    Add API Gateway

--- order-service/src/main/java/fit/se/orderservice/controller/OrderController.java
+++ order-service/src/main/java/fit/se/orderservice/controller/OrderController.java
@@ -1,4 +1,26 @@
 package fit.se.orderservice.controller;
 
+import fit.se.orderservice.model.Order;
+import fit.se.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderRepository orderRepository;
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
 }
--- order-service/src/main/java/fit/se/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/fit/se/orderservice/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package fit.se.orderservice.repository;
 
-public class OrderRepository {
+import fit.se.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
